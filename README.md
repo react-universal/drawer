13746cf3dbf356123d7f1951514abd4a8d25fd6a
codypearce <codyapearce@gmail.com>
2020-02-10 07:02:51 -0800

    Update stories, update readme

--- README.md
+++ README.md
@@ -1,57 +1,114 @@
 <p align="center" style="margin-bottom: 0px !important;">
-  <img width="600" src="https://raw.githubusercontent.com/react-universal/ripple/master/media/logo.jpg" alt="Material Bread logo" align="center">
+  <img width="600" src="https://raw.githubusercontent.com/react-universal/ripple/master/media/logo.jpg" alt="React Universal Component" align="center">
 </p>
 
-
-A React Material Ripple component that works on the Web, Android, iOS, Electron, and anywhere else React Native is supported. This component is a part of the React Universal ecosystem of universal React and React Native components. Checkout the [React Universal docs](https://github.com/react-universal/react-universal) to learn more.
-
+A React Material Drawer component that works on the Web, Android, iOS, Electron, and anywhere else React Native is supported. This component is a part of the React Universal ecosystem of universal React and React Native components. Checkout the [React Universal docs](https://github.com/react-universal/react-universal) to learn more.
 
 ### [Storybook Demos](https://react-universal.github.io/ripple/?path=/story/*)
-
-
 
 ### Installation
 
 ```bash
-npm i @reactuniversal/ripple --save
+npm i @reactuniversal/drawer --save
 ```
 
-### Example
+### Modal Drawer
 
-```
-import React from 'react';
-import { View, Text } from 'react-native';
-import Ripple from '@reactuniversal/ripple';
+<p>
+<img height="600" src="https://i.imgur.com/TSIqKRM.gif" alt="adf" align="center">
+<img height="600" src="https://i.imgur.com/1Ws0IVw.gif" alt="adf" align="center">
+</p>
 
-export default function Page() {
+### Push Drawer
+
+<p>
+<img height="600" src="https://i.imgur.com/nIEd8Gu.gif" alt="adf" align="center">
+<img height="600" src="https://i.imgur.com/IIiMtah.gif" alt="adf" align="center">
+</p>
+
+### Permanent
+
+<p>
+<img src="https://i.imgur.com/6Vb32ht.png" alt="adf" align="center">
+</p>
+
+### Usage
+
+```jsx
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {Drawer} from '@reactuniversal/drawer';
+
+const styles = {
+  container: {
+    width: '100%',
+    height: '100vh',
+  },
+  body: {
+    backgroundColor: '#eee',
+    height: '100vh',
+  },
+};
+
+export default class App extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  render() {
     return (
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-        <Ripple
-          rippleColor={'blue'}
-          style={{
-            width: 180,
-            height: 250,
-          }}>
-          <Text>Click Me</Text>
-        </Ripple>
+      <View style={styles.container}>
+        <Drawer
+          open={this.state.isOpen}
+          drawerContent={
+            <View>
+              <Text>Drawer Content</Text>
+            </View>
+          }
+          onClose={() => this.setState({isOpen: false})}
+          animationTime={250}>
+          <View style={styles.body}>
+            <View
+              style={{
+                marginTop: 20,
+                alignItems: 'center',
+                width: '100%',
+                flex: 1,
+              }}>
+              <Text style={{marginBottom: 20}}>This is a page</Text>
+              <TouchableOpacity
+                onPress={() => this.setState({isOpen: !this.state.isOpen})}>
+                <Text>Toggle</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Drawer>
       </View>
     );
-}`;
-
+  }
+}
 ```
 
-#### Props
+### Props
 
-| Name                  | Description                                                      | Type   | Default |
-| --------------------- | ---------------------------------------------------------------- | ------ | ------- |
-| displayUntilPressOut     | Keep ripple at full size while holding press                           | bool | true    |
-| disabled                 | Disables ripple                                                    | bool |         |
-| onPress           | Callback on ripple                        | func   |         |
-| rippleCentered                | Whether the ripple starts at the center of the component or where you clicked                                | bool   |     false    |
-| rippleColor                | Color of ripple                                    | string | rgba(0, 0, 0, .87)      |
-| rippleContainerBorderRadius | Border Radius of ripple           | number | 0       |
-| rippleDuration                 | Animation Duration of ripple to fill the entire component                                            | duration |    400     |
-| rippleOpacity            | Opacity of ripple                           | number |   0.3      |
-| rippleSize                 | Determines the size of ripple | number |    0     |
-| rippleSequential               | Ripple should start in sequence                                    | bool   |     false    |
-| style     | Styles root element                         | object |     |
+| Name                  | Description                                                                | Type   | Default      |
+| --------------------- | -------------------------------------------------------------------------- | ------ | ------------ |
+| animationTime         | How long the drawer animation should be, in ms                             | number | 200          |
+| appbar                | Render appbar above drawer and page content for clipping purposes          | node   |              |
+| contentContainerStyle | Styles wrapper around page content                                         | object |              |
+| drawerContent         | Components displayed inside the drawer                                     | node   |              |
+| drawerStyle           | Styles drawer                                                              | object |              |
+| fullHeight            | Drawer takes up full height of the page so conent is not scrolled          | bool   | false        |
+| onClose               | Callback when clicking outside of drawer                                   | func   |              |
+| pageHeight            | Override calculated pageHeight, useful for demos as shown                  | number | ScreenHeight |
+| pageWidth             | Override calculated pageWidth, useful pages that do not take up full space | number | ScreenWidth  |
+| position              | Position of drawer                                                         | string | absolute     |
+| direction             | Where should the drawer open: `left` or `right`                            | string | left         |
+| open                  | Whether drawer is shown or hidden                                          | bool   | false        |
+| style                 | Styles container element                                                   | object |              |
+| scrim                 | Whether scrim is shown                                                     | bool   | true         |
+| scrimColor            | Color of scrim                                                             | object | black        |
+| scrimOpacity          | The ending opacity for the scim                                            | number | .4           |
+| type                  | Determines the type of drawer from modal, push, and permanenent            | string | modal        |
+| width                 | Specific drawer width in pixels, will override widthPercentage             | number | 240          |
+| widthPercentage       | Percentage of pageWidth the drawer should take up                          | 0-1    | .40          |
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,178 +1,340 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Platform,
+  Dimensions,
+  ScrollView,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
 
-import Ripple from '../../src/index';
+import Drawer from '../../src/index';
 
-import Header from '../components/Header';
 import Container from '../components/Container';
 import {storiesOf} from '../helpers/storiesOf';
 
-storiesOf('Ripples', module)
-  .addParameters({jest: ['Ripple']})
-  .add('from click', () => (
-    <Container>
-      <Header title={'Ripple from where it was clicked'} />
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-        <Ripple
-          rippleColor={'blue'}
-          style={{
-            width: 180,
-            height: 250,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 1,
-            borderColor: 'black',
-            borderRadius: 5,
-          }}>
-          <Text>Click Me</Text>
-        </Ripple>
-        <Ripple style={{width: 200, height: 200, marginLeft: 40}}>
-          <View
-            style={{
-              width: 200,
-              height: 200,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>Click Me</Text>
-          </View>
-        </Ripple>
-      </View>
-      <View style={{flexDirection: 'row', flexWrap: 'wrap', marginTop: 40}}>
-        <Ripple
-          rippleColor={'#E91E63'}
-          rippleContainerBorderRadius={100}
-          style={{
-            width: 100,
-            height: 100,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <View style={{width: 48, height: 48, backgroundColor: 'red'}}></View>
-        </Ripple>
-        <Ripple
-          rippleColor={'#9C27B0'}
-          style={{
-            width: 300,
-            height: 200,
-            marginLeft: 40,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          rippleContainerBorderRadius={100}>
-          <Text>Massive Button</Text>
-        </Ripple>
-      </View>
-    </Container>
-  ))
-  .add('from center', () => (
-    <Container scroll>
-      <Header title={'Ripple form center'} />
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-        <Ripple
-          rippleColor={'blue'}
-          rippleCentered
-          style={{
-            width: 180,
-            height: 250,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 1,
-            borderColor: 'black',
-            borderRadius: 5,
-          }}>
-          <Text>Click Me</Text>
-        </Ripple>
-        <Ripple
-          rippleCentered
-          style={{width: 200, height: 200, marginLeft: 40}}>
-          <View
-            shadow={4}
-            style={{
-              width: 200,
-              height: 200,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>Click Me</Text>
-          </View>
-        </Ripple>
-      </View>
-    </Container>
-  ))
-  .add('sequential', () => (
-    <Container scroll>
-      <Header title={'Sequential Ripple'} />
-      <Text style={{marginBottom: 40}}>
-        Try clicking quickly versus other demos
-      </Text>
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-        <Ripple
-          rippleColor={'blue'}
-          rippleSequential
-          style={{
-            width: 180,
-            height: 250,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 1,
-            borderColor: 'black',
-            borderRadius: 5,
-          }}>
-          <Text>Click Me</Text>
-        </Ripple>
-        <Ripple
-          rippleCentered
-          rippleSequential
-          style={{width: 200, height: 200, marginLeft: 40}}>
-          <View
-            shadow={4}
-            style={{
-              width: 200,
-              height: 200,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>Click Me</Text>
-          </View>
-        </Ripple>
-      </View>
-    </Container>
-  ))
-  .add('speed', () => (
-    <Container scroll>
-      <Header title={'Speed Ripple'} />
+import {State, Store} from '@sambego/storybook-state';
 
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-        <Ripple
-          rippleColor={'blue'}
-          rippleDuration={250}
-          style={{
-            width: 180,
-            height: 250,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 1,
-            borderColor: 'black',
-            borderRadius: 5,
-          }}>
-          <Text>Really fast</Text>
-        </Ripple>
-        <Ripple
-          rippleCentered
-          rippleDuration={1000}
-          style={{width: 200, height: 200, marginLeft: 40}}>
-          <View
-            shadow={4}
-            style={{
-              width: 200,
-              height: 200,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>Really Slow</Text>
-          </View>
-        </Ripple>
+const store = new Store({
+  isOpen: false,
+  isOpenPermanent: true,
+});
+
+const DrawerContent = () => {
+  return (
+    <View>
+      <Text>React Universal</Text>
+
+      <View>
+        <Text>Item</Text>
+        <Text>Item</Text>
+        <Text>Item</Text>
       </View>
+    </View>
+  );
+};
+
+const PageContent = () => {
+  return (
+    <View style={{marginTop: 20, alignItems: 'center'}}>
+      <Text type={4} style={{marginBottom: 20}}>
+        This is a page
+      </Text>
+      <Text>Click the menu button to open the drawer</Text>
+      <Text>Click outside the drawer to close it</Text>
+    </View>
+  );
+};
+
+const AppbarContent = isOpen => {
+  return (
+    <View style={{width: '100%'}}>
+      <Text>Appbar content</Text>
+    </View>
+  );
+};
+
+const Appbar = children => {
+  return (
+    <View style={{width: '100%', height: 56, backgroundColor: 'green'}}>
+      {children}
+    </View>
+  );
+};
+
+const pageWidth = Platform.OS === 'web' ? 600 : Dimensions.get('window').width;
+
+export default storiesOf('Drawer', module)
+  .addParameters({jest: ['Drawer']})
+  .add('Simple', () => (
+    <Container scroll style={{padding: 0, flex: 1}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              animationTime={250}
+              opacity={0.33}>
+              <View style={styles.body}>
+                <TouchableHighlight
+                  onPress={() => store.set({isOpen: !state.isOpen})}>
+                  <Text>Toggle</Text>
+                </TouchableHighlight>
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Over Appbar', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              animationTime={250}>
+              <View style={styles.body}>
+                <AppbarContent isOpen={state.isOpen} />
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Under Appbar', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              appbar={<AppbarContent isOpen={state.isOpen} />}>
+              <View style={styles.body}>
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('No Scrim', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              pageHeight={500}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              scrim={false}>
+              <View style={styles.body}>
+                <AppbarContent isOpen={state.isOpen} />
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Custom Scrim', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              scrimColor={'#E91E63'}
+              scrimOpacity={0.2}>
+              <View style={styles.body}>
+                <AppbarContent isOpen={state.isOpen} />
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Scroll inner', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              pageHeight={500}
+              drawerContent={
+                <ScrollView>
+                  <View>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                  </View>
+                  <View>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                  </View>
+                  <View>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                  </View>
+                  <View>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                    <Text>Item</Text>
+                  </View>
+                </ScrollView>
+              }
+              onClose={() => store.set({isOpen: false})}
+              scrimColor={'#E91E63'}
+              scrimOpacity={0.2}>
+              <View style={styles.body}>
+                <AppbarContent isOpen={state.isOpen} />
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Push', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              pageHeight={500}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              type={'push'}>
+              <View style={styles.body}>
+                <AppbarContent isOpen={state.isOpen} />
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Push Under', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={styles.container}>
+            <Drawer
+              open={state.isOpen}
+              pageWidth={pageWidth}
+              pageHeight={500}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              type={'push'}
+              appbar={<AppbarContent isOpen={state.isOpen} />}>
+              <View style={styles.body}>
+                <PageContent />
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Permanent', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={[styles.container, {width: 'auto'}]}>
+            <Drawer
+              open={state.isOpenPermanent}
+              pageWidth={
+                Platform.OS == 'web' ? 1000 : Dimensions.get('window').width
+              }
+              pageHeight={500}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              type={'permanent'}>
+              <View
+                style={{
+                  width: '100%',
+                }}>
+                <Appbar />
+                <View style={styles.body}>
+                  <PageContent />
+                </View>
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
+    </Container>
+  ))
+  .add('Permanent Under', () => (
+    <Container scroll style={{padding: 0}}>
+      <State store={store} style={{flex: 1}}>
+        {state => (
+          <View style={[styles.container, {width: 'auto'}]}>
+            <Drawer
+              open={state.isOpenPermanent}
+              pageWidth={
+                Platform.OS == 'web' ? 1000 : Dimensions.get('window').width
+              }
+              pageHeight={500}
+              drawerContent={<DrawerContent />}
+              onClose={() => store.set({isOpen: false})}
+              type={'permanent'}
+              appbar={<Appbar />}>
+              <View
+                style={{
+                  width: '100%',
+                }}>
+                <View style={styles.body}>
+                  <PageContent />
+                </View>
+              </View>
+            </Drawer>
+          </View>
+        )}
+      </State>
     </Container>
   ));
+
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 1,
+    position: 'relative',
+    flex: 1,
+  },
+  body: {
+    width: pageWidth,
+    height: 1000,
+    flex: 1,
+  },
+});
